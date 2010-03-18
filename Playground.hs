0e653a0f9bb55a33747f705ce8c8c43f07957f2c
Thies Heidecke <theidecke@astrophysik.uni-kiel.de>
2010-03-18 17:43:08 +0100

    substituted sampleAbsJacDet with more general sampleImportance, because we're already using it in the metropolis-algorithm

--- Playground.hs
+++ Playground.hs
@@ -97,14 +97,14 @@ module Playground where
 
   data Gauss2DCartesian = Gauss2DCartesian
   instance MetropolisDistribution Gauss2DCartesian (Double,Double) where
-    constructSampleWithImportance _ ((u1:u2:_):_) = Just (toSampled value importance) where
+    constructSampleWithImportance _ ((u1:u2:_):_) = Just $ value `withImportance` importance where
       value = (x,y)
       (x,y) = (2*u1-1,2*u2-1)
       importance = exp . negate . (30*) $ x^2 + y^2
 
   data Gauss2DPolar = Gauss2DPolar
   instance MetropolisDistribution Gauss2DPolar (Double,Double) where
-    constructSampleWithImportance _ ((u1:u2:_):_) = Just (toSampled value importance) where
+    constructSampleWithImportance _ ((u1:u2:_):_) = Just $ value `withImportance` importance where
       importance = contribution / probability
       contribution = exp . negate . (30*) $ r^2
       value = (r*(cos phi),r*(sin phi))
--- Sampled.hs
+++ Sampled.hs
@@ -6,8 +6,8 @@ module Sampled (
     getCoord,
     Sampled,
     sampledValue,
-    sampledAbsJacDet,
-    withAbsJacDet,
+    sampledImportance,
+    withImportance,
     withProbability,
     andWith
   ) where
@@ -19,17 +19,20 @@ module Sampled (
     -- | construct a sampler which yields "b"s from an "a" using a stream of unitcoordinates
     sampleFrom :: a -> UCStreamTo (Sampled b)
 
-  -- | sample z of type a with the absolute jacobian determinant of its transformation from unitcubespace.
-  -- | this equals the reciprocal of the probability density |det(dS(u)/du)| = 1/p_S(u), z = S(u)
+  -- | sample z of type a with its importance prepended. The importance is the product of
+  -- | sample-contribution and the absolute jacobian determinant of its transformation from unitcubespace
+  -- | i = c*ajd = c/p
+  -- | which equals the reciprocal of the probability density |det(dS(u)/du)| = 1/p_S(u), z = S(u)
+
   type Sampled a = (Double, a)
   
   sampledValue = snd
-  sampledAbsJacDet = fst
-  withAbsJacDet v ajd = (ajd,v)
+  sampledImportance = fst
+  withImportance v ajd = (ajd,v)
   withProbability v p = (1/p,v)
   
-  andWith f s1 s2 = (v1 `f` v2) `withAbsJacDet` (ajd1 * ajd2) where
+  andWith f s1 s2 = (v1 `f` v2) `withImportance` (i1 * i2) where
     v1 = sampledValue s1
     v2 = sampledValue s2
-    ajd1 = sampledAbsJacDet s1
-    ajd2 = sampledAbsJacDet s2
+    i1 = sampledImportance s1
+    i2 = sampledImportance s2
