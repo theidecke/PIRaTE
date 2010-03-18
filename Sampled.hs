e9b7077ef714a73eab9e8b1d2408f8daefc0c5f5
Thies Heidecke <theidecke@astrophysik.uni-kiel.de>
2010-03-18 17:20:57 +0100

    changed some method names in Sampled module

--- Sampled.hs
+++ Sampled.hs
@@ -1,21 +1,35 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 
-module Sampled where
+module Sampled (
+    Sampleable(..),
+    UCStreamTo,
+    getCoord,
+    Sampled,
+    sampledValue,
+    sampledAbsJacDet,
+    withAbsJacDet,
+    withProbability,
+    andWith
+  ) where
 
   import Control.Monad.State
+  import UCStream (UCStreamTo,getCoord)
 
   class Sampleable a b where
-    randomSampleFrom    :: a -> State s (Sampled b)
+    -- | construct a sampler which yields "b"s from an "a" using a stream of unitcoordinates
+    sampleFrom :: a -> UCStreamTo (Sampled b)
 
-  -- | value of type a with its probability density
+  -- | sample z of type a with the absolute jacobian determinant of its transformation from unitcubespace.
+  -- | this equals the reciprocal of the probability density |det(dS(u)/du)| = 1/p_S(u), z = S(u)
   type Sampled a = (Double, a)
   
   sampledValue = snd
-  sampledImportance = fst
-  toSampled v i = (i,v)
+  sampledAbsJacDet = fst
+  withAbsJacDet v ajd = (ajd,v)
+  withProbability v p = (1/p,v)
   
-  andWith f s1 s2 = toSampled (v1 `f` v2) (i1 * i2) where
+  andWith f s1 s2 = (v1 `f` v2) `withAbsJacDet` (ajd1 * ajd2) where
     v1 = sampledValue s1
     v2 = sampledValue s2
-    i1 = sampledImportance s1
-    i2 = sampledImportance s2
+    ajd1 = sampledAbsJacDet s1
+    ajd2 = sampledAbsJacDet s2
--- UCStream.hs
+++ UCStream.hs
@@ -1,4 +1,4 @@
-{-# Language ParallelListComp #-}
+{-# LANGUAGE ParallelListComp #-}
 
 module UCStream (
     UCStream,
